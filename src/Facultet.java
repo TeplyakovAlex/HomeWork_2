c84744eab4e3bb4d3e87cd69fda421194fec46d0
Alexandr Teplyakov <Teplyakov.in@gmail.com>
2016-05-24 12:53:11 +0300

    first

---
+++ src/Facultet.java
@@ -0,0 +1,13 @@
+public class Facultet {
+
+    private final String name;
+
+    Facultet(String name){
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main (String[]args){
+        Facultet facult_tks = new Facultet("TKS");
+        Facultet facult_is = new Facultet("IS");
+        Facultet[] facultets = new Facultet[2];
+        facultets[0] = facult_tks;
+        facultets[1] = facult_is;
+
+        Student[] students = new Student[]{new Student(18,"Vasya", facult_is), new Student(19,"Vanya", facult_tks)};
+
+        Institute institute = new Institute(facultets, students);
+
+        System.out.print(institute.getInformation());
+    }
+}
